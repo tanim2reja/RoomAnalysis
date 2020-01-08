9b530cab008f01d7aa0764faaae7b920fbad1c56
tanim_reja <tanim2reja@gmail.com>
2020-01-08 17:36:27 +0600

    firt commit

---
+++ app/src/main/java/com/jolpai/room/entity/Word.kt
@@ -0,0 +1,10 @@
+package com.jolpai.room.entity
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName="word_table")
+data class Word (@PrimaryKey @ColumnInfo(name="id") val id: Int,
+                 @ColumnInfo(name="word") val word: String,
+                 @ColumnInfo(name="meaning") val meaning: String)
